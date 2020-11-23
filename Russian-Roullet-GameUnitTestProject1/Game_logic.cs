4557565913b5025683bd8b0239cc423082b00f63
Sukhdeep Dhillon <SD042@atcltd.atc.org.nz>
2020-11-24 09:50:23 +1300

    update

---
+++ Russian-Roullet-GameUnitTestProject1/Game_logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Russian_Roullet_GameUnitTestProject1
+{
+    internal class Game_logic
+    {
+        internal int load_Position;
+        internal int load_position;
+        internal int spin_position;
+        internal int spin_Position;
+
+        internal int shoot_method()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Russian-Roullet-GameUnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Russian-Roullet-GameUnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Russian-Roullet-GameUnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b33c1b12-a7bb-4bf3-aa4b-294f1d71e43a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
